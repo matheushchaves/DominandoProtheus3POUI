81ac9f24de7a3e07badac3a6590bb5d314995e97
DELL\Matheus <matheushchaves@outlook.com>
2023-10-16 19:06:09 -0300

    crud

--- src/front/my-po-project/src/app/crud/crud.component.ts
+++ src/front/my-po-project/src/app/crud/crud.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PoTableColumn } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-crud',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./crud.component.css']
 })
 export class CrudComponent {
-
+  filteredColumns: Array<string> = [ 'c0a_id', 'c0a_codigo', 'c0a_exncm', 'c0a_descri', 'c0a_perncm', 'c0a_valida', 'c0a_stamp']
+  columns: Array<PoTableColumn> = [
+    { property: 'c0a_filial', label: 'Filial', type: 'string' },
+    { property: 'c0a_id', label: 'ID', type: 'string' },
+    { property: 'c0a_codigo', label: 'Cod. NCM', type: 'string' },
+    { property: 'c0a_exncm', label: 'Ex-NCM', type: 'string' },
+    { property: 'c0a_descri', label: 'Descr NCM', type: 'string' },
+    { property: 'c0a_perncm', label: 'Percentual', type: 'string' },
+    { property: 'c0a_valida', label: 'Data Vigenc.', type: 'string' },
+    { property: 'c0a_stamp', label: 'Stamp', type: 'string' }
+  ]
 }
